3fbddabf54b7457966afc76b55324950d942fdc9
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2025-09-10 19:08:46 +0800

    feat: setup necesarry components

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,15 @@
 import './assets/css/style.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Button from "./Button"
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="p-5 text-center bg-light-dark rounded">
+                    <h1 className="text-light">Stock Prediction Portal</h1>
+                    <p className="text-light lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ad explicabo ducimus repellat ab nostrum a ut? Eveniet quisquam quae corporis, nisi ipsam laboriosam praesentium repellendus laborum obcaecati alias cupiditate!</p>
+                    <Button text="Login" class="btn-outline-info"> </Button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
