2a1bcf9de597fd602ecb5e8d464a08c2b0be8ca3
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2025-09-11 18:51:06 +0800

    feat: created registration component and implement routing

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import './assets/css/style.css'
-import Header from './components/Header'
 import Home from './components/Home'
+import Register from './components/Register'
+import Header from './components/Header'
 import Footer from './components/Footer'
-
 function App() {
 
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <Header> </Header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      <Footer> </Footer>
     </>
   )
 }
