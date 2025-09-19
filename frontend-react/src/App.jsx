8451c0ae4a05e1da1590ad27ee758b74e434fddc
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2025-09-19 20:54:49 +0800

    feat: create dashboard component

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./components/Register"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Login from "./components/Login"
+import Dashboard from "./components/dashboard/Dashboard"
 function App() {
 
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
           <Footer> </Footer>
         </BrowserRouter>
---
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+function Dashboard() {
+
+    return (
+        <>
+            <div className="container text-light" >Dashboard</div>
+        </>
+    )
+}
+
+export default Dashboard;
