3d0328184c6b6aef05e7d0788af9c607b35a8187
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2026-01-20 21:57:30 +0800

    feat: accept-stock-ticker

--- frontend-react/src/components/Home.jsx
+++ frontend-react/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <div className="container">
                 <div className="p-5 text-center bg-light-dark rounded">
                     <h1 className="text-light">Stock Prediction Portal</h1>
-                    <p className="text-light lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ad explicabo ducimus repellat ab nostrum a ut? Eveniet quisquam quae corporis, nisi ipsam laboriosam praesentium repellendus laborum obcaecati alias cupiditate!</p>
+                    <p className="text-light lead">This stock prediction application utilizes machine learning techniques, specifically emplying Keras, and LTM model, integrated within the Django framework. It forecasts future stock prices by analyzing 100-day and 200-day moving averages, essential indicators widely used by stock analysts to inform trading and investment decisions.</p>
                     <Button to="/dashboard" text="Explore Now" class="btn-outline-info"> </Button>
                 </div>
             </div>
