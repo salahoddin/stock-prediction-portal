fde08f8f7c9aaa10772d4195d2545c7cb3c840ee
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2026-01-21 21:27:03 +0800

    fix: add spinner while fetching data

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../../axiosInstance";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 function Dashboard() {
     const [ticker, setTicker] = useState('')
     const [statusMessage, setStatusMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchProtectedData = async () => {
@@ -18,14 +22,21 @@ function Dashboard() {
     }, []);
 
     const handleSubmit = async (event) => {
+        setLoading(true);
         event.preventDefault();
         try {
             const response = await axiosInstance.post("/predict/", { ticker });
             console.log(response.data);
             setStatusMessage(response.data?.status ?? "success");
+            setErrorMessage("");
         } catch (error) {
-            console.error(error);
-            setStatusMessage("error");
+            console.log("API error:", error.response?.data);
+            const apiStatus = error?.response?.data?.status;
+            const apiMessage = error?.response?.data?.message;
+            setErrorMessage(apiMessage || "error");
+            setStatusMessage(apiMessage || apiStatus || "error");
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -36,7 +47,10 @@ function Dashboard() {
                     <div className="col-md-6 mx-auto">
                         <form onSubmit={handleSubmit}>
                             <input type="text" className="form-control" placeholder="Enter Stock Ticker" onChange={(e) => setTicker(e.target.value)} />
-                            <button type="submit" className="btn btn-info mt-3">See Prediction</button>
+                            <small>{errorMessage && <div className="text-danger">{errorMessage}</div>}</small>
+                            <button type="submit" className="btn btn-info mt-3">
+                                {loading ? <span><FontAwesomeIcon icon={faSpinner} spin></FontAwesomeIcon>Please Wait...</span> : "See Prediction"}
+                            </button>
                         </form>
                         {statusMessage && <div className="mt-2">{statusMessage}</div>}
                     </div>
