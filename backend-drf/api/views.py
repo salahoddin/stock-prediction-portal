a48365d8837c49e3908b15ec10a28b58b7bf289b
Salahod-din Sambutuan <salahoddin.sambutuan89@gmail.com>
2026-01-21 19:31:19 +0800

    feat: add view for fetching data in yfinance and create dedicated module for ML logic

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -3,6 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import StockPredictionSerializer
+from .ml.utils import download_stock_data
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Input
 
 
 # Create your views here.
@@ -11,6 +20,13 @@ class StockPredictionAPIView(APIView):
         serializer = StockPredictionSerializer(data=request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data["ticker"]
+            df = download_stock_data(ticker)
+            if df.empty:
+                return Response(
+                    {"status": "error", "message": "Invalid ticker"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            print(df.head())
             return Response(
                 {"status": "success from django", "ticker": ticker},
                 status=status.HTTP_200_OK,
